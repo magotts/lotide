b445c4423bb06e34838e1c9fc70c33024111c48b
magotts <techybet@gmail.com>
2021-08-19 20:55:01 -0400

    map.js

---
+++ map.js
@@ -0,0 +1,38 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
+  } else
+    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const map = function(array, callback) {
+  // console.log('array: ', array);
+  // console.log('callback: ', callback);
+
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
+
+
