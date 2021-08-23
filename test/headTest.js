3e4c05531b32275493c0e46b60f885fffd201a3e
magotts <techybet@gmail.com>
2021-08-23 14:26:52 -0400

    refactored headTest. added mocha and chai.

---
+++ test/headTest.js
@@ -0,0 +1,20 @@
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns '6' for ['6']", () => {
+    assert.strictEqual(head([6, 4, 7]), 6);
+  });
+
+  it("returns 'Hello' for ['Hello']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+});
