82379392c6e5a245dd91d60a128ac8cf5aeae338
magotts <techybet@gmail.com>
2021-08-23 13:47:08 -0400

    refactored assertArraysEqual. added the eqArrays require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1,arr2)) {
@@ -6,12 +8,5 @@ const assertArraysEqual = function(arr1, arr2) {
     console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
 };
 
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  } return true;
-};
 
 module.exports = assertArraysEqual;
