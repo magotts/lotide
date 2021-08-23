d3419434489a12691027e0feb1904a6eeb9a8e45
magotts <techybet@gmail.com>
2021-08-23 14:01:19 -0400

    refactored middle. added module.exports

--- middle.js
+++ middle.js
@@ -30,11 +30,5 @@ const middle = function(array) {
   return newMid;
 };
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+module.exports = middle;
